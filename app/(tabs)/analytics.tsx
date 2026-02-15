77d407ab49e4625c0aa4b1e51292bf6387ee380c
nishuldhakar <nishuldhakar.se@gmail.com>
2026-02-15 13:17:06 +0530

    created tab-navigation

---
+++ app/(tabs)/analytics.tsx
@@ -0,0 +1,25 @@
+import { Colors } from "@/constants/colors";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function AnalyticsScreen() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Analytics</Text>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.neutral[50],
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: Colors.neutral[900],
+    },
+});
